9b4e9f80bde64dbd84813644c045e8667601a6f7
Pedroguibas <pedroguibas123@gmail.com>
2025-09-04 11:57:07 -0300

    Adds Images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Layout } from "./components/shared/layout";
 import Carousel from "./components/Carousel";
+import OngsCarousel from './components/OngsCarousel';
 
 function App() {
     return (
         <Layout>
             <div className="md:col-span-10 md:col-start-2">
                 <Carousel />
+                <OngsCarousel />
             </div>
         </Layout>
     );
---
+++ src/components/OngsCarousel.tsx
@@ -0,0 +1,34 @@
+import amigosDosNoivos from '../assets/logo/ongs/amigosdosnoivos.png';
+import amigosSemDono from '../assets/logo/ongs/amigosemdono.png';
+import brigada from '../assets/logo/ongs/brigadadosanimaissemteto.png';
+import miadosELatidos from '../assets/logo/ongs/confrariadosmiadoselatidos.png';
+import focinhosDeLuz from '../assets/logo/ongs/focinhosdeluz.png';
+import euSouOBixo from '../assets/logo/ongs/institutoeusouobixo.png';
+import laticao from '../assets/logo/ongs/laticao.png';
+import protetores from '../assets/logo/ongs/protetoresvoluntarios.png';
+import viralata from '../assets/logo/ongs/viralataviraamigo.jpg';
+
+
+
+
+function OngsCarousel() {
+
+    const images = [
+        amigosDosNoivos,
+        amigosSemDono,
+        brigada,
+        miadosELatidos,
+        focinhosDeLuz,
+        euSouOBixo,
+        laticao,
+        protetores,
+        viralata
+    ]
+
+    return (
+        <section className="mt-15">
+        </section>
+    )
+}
+
+export default OngsCarousel;
