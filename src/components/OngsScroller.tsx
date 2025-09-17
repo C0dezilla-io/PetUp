41dbbc00aabb7895cc29320623fbd58acfe3315b
Pedroguibas <pedroguibas123@gmail.com>
2025-09-17 09:35:16 -0300

    Fix: Ongs section code flexibility
    The ongs images were hardcoded into the .tsx file. This update implements an ongs json so the values for the ong's name and website href are stored and can be built dynamically

--- src/components/OngsScroller.tsx
+++ src/components/OngsScroller.tsx
@@ -1,35 +1,18 @@
 import "../css/ongsScroller.css";
 
-import amigosDosNoivos from "../assets/logo/ongs/amigosdosnoivos.png";
-import brigada from "../assets/logo/ongs/brigadadosanimaissemteto.png";
-import miadosELatidos from "../assets/logo/ongs/confrariadosmiadoselatidos.png";
-import focinhosDeLuz from "../assets/logo/ongs/focinhosdeluz.png";
-import euSouOBixo from "../assets/logo/ongs/institutoeusouobixo.png";
-import protetores from "../assets/logo/ongs/protetoresvoluntarios.png";
-
-// Idealmente, será implementado no banco de dados uma sessão para o usuário armazenar as ongs parceiras que aparecerão no slider
-// Dessa forma será armazenado os links dos sites das ongs e seus nomes para colocar na href do link e no alt da imagem
+import ongs from  "../assets/ongs.json";
 
 function ScrollerItemsList() {
 
-    const images = [
-        amigosDosNoivos,
-        brigada,
-        miadosELatidos,
-        focinhosDeLuz,
-        euSouOBixo,
-        protetores,
-    ];
-
     
     return (
-        images.map((i, index) => (
+        ongs.map((o, index) => (
             <li key={index}>
                 <a
-                    href="https://github.com/C0dezilla-io"
+                    href={o.site}
                     className="w-[calc(100vw/3)] sm:w-[calc(100vw/6)] flex justify-center items-center rounded-[5px] bg-[hsl(210,44%,96%)]"
                 >
-                    <img src={i} alt="Ong parceira" className="w-3/4" />
+                    <img src={`/ongs/${o.imagem}`} alt={o.nome} className="w-3/4" />
                 </a>
             </li>
         ))
