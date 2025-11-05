db0f4231a75f8f5d4ab9b18b5e560a0e8bcded84
Pedroguibas <pedroguibas123@gmail.com>
2025-11-05 18:12:30 -0300

    chore: implements not found page

--- src/components/pages/DoarPets.tsx
+++ src/components/pages/DoarPets.tsx
@@ -1,6 +1,6 @@
 export default function DoarPets() {
   return (
-    <div className="h-vh flex items-center justify-center">
+    <div className="h-lvh flex items-center justify-center">
       <h1>Página de Doar Pets</h1>
     </div>
   );
---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="h-lvh flex flex-col items-center">
+      <h1 className="text-9xl mt-28">
+        <b>404</b>
+      </h1>
+      <h2 className="text-2xl">Página não encontrada.</h2>
+    </div>
+  );
+}
