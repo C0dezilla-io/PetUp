570a8a7b5f52f80f54cff4cbbe00f5dfa28b076d
Pedroguibas <pedroguibas123@gmail.com>
2025-11-08 16:57:37 -0300

    feat: implements modal to show pet information

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -1,6 +1,12 @@
 import { GenderFemale, GenderMale } from "react-bootstrap-icons";
 
-type petType = {
+export type ongType = {
+  nome: string;
+  cidade: string;
+  estado: string;
+};
+
+export type petType = {
   id: number;
   nome: string;
   especie: string;
@@ -11,42 +17,42 @@ type petType = {
   sexo: string;
   sobre: string;
   image: string;
+  ong: ongType;
   adotado: boolean;
 };
 
 interface PetCardPropsType {
   pet: petType;
+  petModalFunction: (pet: petType) => void;
 }
 
-export default function PetCard({ pet }: PetCardPropsType) {
+export default function PetCard({ pet, petModalFunction }: PetCardPropsType) {
+  const handleClick = () => {
+    petModalFunction(pet);
+  };
+
   return (
-    <button className="flex flex-col bg-[var(--secondary-bg)] border rounded-xl border-[var(--text-primary)] shadow-2xl overflow-hidden group hover:-translate-y-1.5 transition-all duration-300 cursor-pointer">
+    <button
+      onClick={handleClick}
+      className="flex flex-col bg-[var(--secondary-bg)] border rounded-xl border-[var(--text-primary)] shadow-2xl overflow-hidden group hover:-translate-y-1.5 transition-all duration-300 cursor-pointer"
+    >
       <div className="aspect-square flex justify-center intems-center overflow-hidden">
         <img
           src={pet.image}
           alt=""
-          className="aspect-square w-full group-hover:scale-110 transition-transform duration-300 ease-out"
+          className="aspect-square object-cover w-full group-hover:scale-110 transition-transform duration-300 ease-out"
         />
       </div>
-      <div className="cardInfo grid grid-cols-2 text-sm text-[var(--secondary-text)] mt-2 p-2 text-left">
-        <div className="flex col-span-2 justify-between">
-          <h2 className="text-[var(--text)] text-base max-w-[80%]">
-            {pet.nome}
-          </h2>
+      <div className="cardInfo flex flex-col text-[var(--secondary-text)] mt-2 p-2 text-left">
+        <div className="flex justify-between">
+          <h2 className="text-[var(--text)] max-w-[80%]">{pet.nome}</h2>
           {pet.sexo == "F" ? (
             <GenderFemale className="text-xl text-pink-400" />
           ) : (
             <GenderMale className="text-xl text-blue-700" />
           )}
         </div>
-        <span className="mt-2">Peso: {pet.peso}</span>
-        <span className="mt-2">Porte: {pet.porte}</span>
-        <span className="mt-2">Idade: {pet.idade}</span>
-        <span className="mt-2">Raça: {pet.raca}</span>
-        <span className="mt-4 text-[var(--text)] col-span-2 text-base">
-          Sobre: <br />
-          {pet.sobre}
-        </span>
+        <span className="mt-4 mb-2 text-sm">{`${pet.ong.nome}, ${pet.ong.cidade} - ${pet.ong.estado}`}</span>
       </div>
     </button>
   );
--- src/components/pages/Pets.tsx
+++ src/components/pages/Pets.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import PetCard from "../PetCard.tsx";
+import PetModal from "../PetModal.tsx";
+import type { petType } from "../PetCard.tsx";
 
 export default function Pets() {
   const testPets = [
@@ -14,6 +17,11 @@ export default function Pets() {
       sobre: "Matará, se necessário.",
       image:
         "https://i.pinimg.com/1200x/c3/6c/5c/c36c5c41e74df5bbe5e557c5ce8f210f.jpg",
+      ong: {
+        nome: "ongX",
+        cidade: "Marília",
+        estado: "SP",
+      },
       adotado: false,
     },
     {
@@ -28,9 +36,20 @@ export default function Pets() {
       sobre: "Tem medo de tudo mas é extremamente carinhosa!",
       image:
         "https://i.pinimg.com/564x/3e/51/a5/3e51a5de3ed1d5f35d90199d0af2c574.jpg",
+      ong: {
+        nome: "ongX",
+        cidade: "Marília",
+        estado: "SP",
+      },
       adotado: false,
     },
   ];
+
+  const [currentPet, setCurrentPet] = useState<petType | null>(null);
+
+  const handlePetClick = (pet: petType) => {
+    setCurrentPet(pet);
+  };
 
   return (
     <section className="grid grid-cols-12">
@@ -38,9 +57,10 @@ export default function Pets() {
         <h1 className="text-2xl mt-12">Pets Disponíveis</h1>
         <ul className="mt-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
           {testPets.map((pet, i) => (
-            <PetCard pet={pet} key={i} />
+            <PetCard pet={pet} petModalFunction={handlePetClick} key={i} />
           ))}
         </ul>
+        <PetModal pet={currentPet} />
       </div>
     </section>
   );
